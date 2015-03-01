ed74a1ed6d64854c5d22cb58cc02587f4216146a
ruslashev <ruslashev@gmail.com>
2015-03-01 20:38:30 +0300

    Apply example from the SDL_Audio page, which works

--- main.cpp
+++ main.cpp
@@ -2,20 +2,28 @@
 #include <SDL2/SDL_audio.h>
 #include <math.h>
 
-const int Amplitude = 28000;
-const int Frequency = 44100;
-unsigned int time;
-int total_length;
+// const int Amplitude = 28000;
+const int DesiredFrequency = 44100;
+const int DesiredSamples = 4096;
+unsigned int SampleFrequency = 0;
+
+unsigned int phase = 0;
 
 void audio_callback(void *userdata, Uint8 *stream, int length)
 {
-	Sint16 *buffer = (Sint16*)stream;
-	length /= 2;
+	unsigned int bytes_per_period = SampleFrequency / 1000;
+
 	for (int i = 0; i < length; i++) {
-		buffer[i] = Amplitude * sin(2*M_PI*time);
-		time++;
+		int value = (int)(150*sin(phase*2*M_PI / bytes_per_period));
+		if (value > 127)
+			value = 127;
+        if (value < -128)
+			value = -128;
+		stream[i] = value;
+		// phase = (++phase) % bytes_per_period;
+		phase++;
+		phase %= bytes_per_period;
 	}
-	total_length -= length;
 }
 
 int main()
@@ -24,23 +32,46 @@ int main()
 
 	SDL_AudioSpec desired, obtained;
 
-	desired.freq = Frequency;
-	desired.format = AUDIO_S16SYS;
+	desired.freq = DesiredFrequency;
+	desired.format = AUDIO_S8;
 	desired.channels = 1;
-	desired.samples = 4096;
+	desired.samples = DesiredSamples;
 	desired.callback = audio_callback;
+	desired.userdata = NULL;
 
-	SDL_OpenAudio(&desired, &obtained);
+	if (SDL_OpenAudio(&desired, &obtained) < 0)
+		return 2;
 
-	printf("Frequency: %d\nSamples: %d\n", obtained.freq, obtained.samples*2);
+	SampleFrequency = obtained.freq;
 
-	total_length = Frequency*2;
-	time = 0;
+	printf("Frequency: %d\n", SampleFrequency);
+	if (DesiredSamples != obtained.samples)
+		printf("Samples requested: %d\nSamples obtained: %d\n",
+				DesiredSamples, obtained.samples);
 
 	SDL_PauseAudio(0);
 
-	while (total_length > 0)
-		SDL_Delay(100);
+	SDL_Event event;
+	bool running = true;
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_KEYDOWN:
+					switch (event.key.keysym.sym) {
+						case SDLK_ESCAPE:
+							running = false;
+							break;
+						default: break;
+					}
+					break;
+				case SDL_QUIT:
+					running = false;
+					break;
+			}
+			SDL_Delay(1);
+		}
+		SDL_Delay(1);
+	}
 
 	SDL_CloseAudio();
 	SDL_Quit();
